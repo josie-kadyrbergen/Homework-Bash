dec3e776e666b6e17bd7321ea842d988b5a51682
josie-kadyrbergen <136125446+josie-kadyrbergen@users.noreply.github.com>
2024-05-20 04:35:05 -0500

    Update calculator.sh

--- Homework9/calculator.sh
+++ Homework9/calculator.sh
@@ -3,10 +3,10 @@
 read -p "Enter the first number: " num1
 read -p "Enter the second number: " num2
 
-sum=$((num1+num2))
-difference=$((num1-num2))
-product=$((num1*num2))
-quotient=$((num1/num2))
+sum=$((num1 + num2))
+difference=$((num1 - num2))
+product=$((num1 * num2))
+quotient=$((num1 / num2))
 
 echo "Sum: $sum"
 echo "Difference: $difference"
